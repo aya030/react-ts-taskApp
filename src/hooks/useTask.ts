4a3d3983b85a9b861550a786b7f27c2bcfa467dc
ayn030 <kojinco18.033@gmail.com>
2022-09-19 10:10:19 +0900

    atomの作成、recoilの導入

--- src/hooks/useTask.ts
+++ src/hooks/useTask.ts
@@ -14,11 +14,11 @@ export const useTask = () => {
   }, []);
 
   // Taskの追加
-  const addTaskListItem = (taskContent: string) => {
+  const addTaskListItem = (todoContent: string) => {
     //ulid()で一意のIdを取得
-    const newTaskItem = { id: ulid(), task: taskContent, done: false };
+    const newTaskItem = { id: ulid(), task: todoContent, done: false };
 
-    taskData.addTaskData(newTaskItem).then((addTask: Task) => {
+    taskData.addTaskData(newTaskItem).then((addTask:Task) => {
       setTaskList([addTask, ...taskList]);
     });
   };
